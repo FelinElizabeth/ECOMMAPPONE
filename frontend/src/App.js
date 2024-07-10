729dedea1b8d9c33a39b53119a56283028ee4e31
FelinElizabeth <felinelizabethbinoy@gmail.com>
2024-07-10 11:12:13 +0530

    created the base

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">MY Ecomm App One</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
